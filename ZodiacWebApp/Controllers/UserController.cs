b697d4b9ca597f3e1f8039ffd47fd9cb1df9364a
ecehanUzgun <ecehan766@gmail.com>
2024-09-26 14:15:26 +0300

    User Added

---
+++ ZodiacWebApp/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZodiacWebApp.Models;
+
+namespace ZodiacWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> users = new List<User>()
+        {
+            new User{ ID = 1, Email = "user@gmail.com", Password = "1234"}
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User newUser)
+        {
+            User user = users.FirstOrDefault(x => x.Email == newUser.Email && x.Password == newUser.Password);
+            if (user != null) 
+            { 
+                return RedirectToAction("Welcome");
+            }
+            return View();
+        }
+
+        public IActionResult Welcome() 
+        {
+            return View();
+        }
+    }
+}
