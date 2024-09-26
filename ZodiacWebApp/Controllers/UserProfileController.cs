1f47167fea20247f9351bc1ba7fdd0f56d939adc
ecehanUzgun <ecehan766@gmail.com>
2024-09-26 16:26:22 +0300

    .

--- ZodiacWebApp/Controllers/UserProfileController.cs
+++ ZodiacWebApp/Controllers/UserProfileController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ZodiacWebApp.Models;
 
 namespace ZodiacWebApp.Controllers
 {
     public class UserProfileController : Controller
     {
+        static List<UserProfile> userProfiles = new List<UserProfile>()
+        {
+            new UserProfile{ ID = 1 , Firstname = "Ecehan", Lastname = "Üzgün"}
+        };
+
         public IActionResult Index()
         {
-            return View();
+            return View(userProfiles);
         }
+
+        
     }
 }
